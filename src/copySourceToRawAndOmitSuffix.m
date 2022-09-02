eac2a86131fe67ab118c4c5fcc88fb3943e3a7eb
Ceren <battal.ceren@gmail.com>
2022-09-02 16:24:38 +0200

    source to raw scripts

---
+++ src/copySourceToRawAndOmitSuffix.m
@@ -0,0 +1,17 @@
+function copySourceToRawAndOmitSuffix(subject)
+
+% remove date suffix + copy source to raw 
+% add bids repo
+run ../lib/CPP_BIDS/checkCppBidsDependencies.m;
+
+% remove data suffix
+filter = struct('sub', subject);
+
+% copy source to raw + remove date suffix
+cfg.dir.output = '/Users/battal/Cerens_files/fMRI/Processed/MoebiusProject/';
+convertSourceToRaw(cfg, 'filter', filter)
+
+% remove cpp-bids repo now to prevent repos compatibility issues
+rmpath(genpath('../lib/CPP_BIDS/'));
+
+end
