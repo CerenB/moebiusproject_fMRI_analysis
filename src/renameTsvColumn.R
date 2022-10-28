5ea2d67e2125d95113a958a47d2ecf7a6fe860e3
Ceren <battal.ceren@gmail.com>
2022-10-28 10:40:03 +0200

    adapting renaming tsv files
    with auditory cues

--- src/renameTsvColumn.R
+++ src/renameTsvColumn.R
@@ -12,49 +12,50 @@ source(pathToFunc)
 
 
 bidsRoot <- '/Users/battal/Cerens_files/fMRI/Processed/MoebiusProject/derivatives/cpp_spm/sub-pil009' 
-# taskName <- 'somatotopy' 
-taskName <- 'mototopy'
+taskName <- 'somatotopy'
+# taskName <- 'mototopy'
 
 taskEventsFiles <- bidsr_queryEvents(bidsRoot = bidsRoot, 
                                      taskName = taskName)
 
+data <- NULL
+tsv <- NULL
 
 # for loop to make auditory cue in the trial_type to be modeled in GLM
 for (i in 1:length(taskEventsFiles)) {
-  
+
   tsv <- read.table(paste(bidsRoot, taskEventsFiles[i], sep = '/'), header = TRUE)
-  
+
   # trial_type as subCueOnset label
   # duration as subCueDuration
   # onset as subCueOnset value
-  
+
   # control point = subjectCue already in the column, then do not do it again
   if (length(grep('block_subjectCue', tsv$trial_type)) ==0){
     print('...in progress')
-    
-    # 1. subset the data 
+
+    # 1. subset the data
     data<- tsv[grep('block', tsv$trial_type), ]
-    
-    # # # # 
-    # add if condition, if mototopy, data$cueOnset and data$cueDuration
-    
-    # # # # 
+
+    # # # #
+    # if condition, if mototopy, data$cueOnset and data$cueDuration
+    # # # #
     if(taskName =='mototopy') {
       # 2. reassign the values
       data$onset <- data$cueOnset
       data$duration <- data$cueDuration
-      
+
     } else{
       # 2. reassign the values
       data$onset <- data$subCueOnset
       data$duration <- data$subCueDuration
     }
-    
-    data$trial_type<-'block_subjectCue'
-  
+
+    data$trial_type<-paste(str_sub(data$trial_type,7), 'subjectCue', sep = '_')
+
     # 3. combine with tav and order
     tsv <- rbind(tsv,data)
-  
+
     write.table(tsv,
                 paste(bidsRoot, taskEventsFiles[i], sep = '/'),
                 row.names = FALSE,
@@ -63,6 +64,64 @@ for (i in 1:length(taskEventsFiles)) {
   } else {
     print('nothing to do here')
   }
-  
+
 }
 
+
+
+# # for loop to make half of forehead trials as regressor-of-no-interest
+# for (i in 1:length(taskEventsFiles)) {
+#   
+#   tsv <- read.table(paste(bidsRoot, taskEventsFiles[i], sep = '/'), header = TRUE)
+#   
+#   # control point = subjectCue already in the column, then do not do it again
+#   if (length(grep('block_forehead2', tsv$trial_type)) ==0){
+#     print('...in progress')
+#     
+#     # 1. subset the data 
+#     data<- tsv[grep('block_forehead', tsv$trial_type), ]
+#     
+#     # find even indices  of forehead
+#     all_forehead_idx <-  which(tsv$trial_type=='block_forehead')
+#     even_idx <- all_forehead_idx[c(FALSE, TRUE)]
+#     
+#     # assign the new trial type name
+#     tsv$trial_type[even_idx] <-'block_forehead2'
+# 
+#     # save into .tsv file
+#     write.table(tsv,
+#                 paste(bidsRoot, taskEventsFiles[i], sep = '/'),
+#                 row.names = FALSE,
+#                 sep = '\t',
+#                 quote = FALSE)
+#   } else {
+#     print('nothing to do here')
+#   }
+#   
+# }
+
+
+# # for loop to reserve the forehead2 --> forehead trials 
+# for (i in 1:length(taskEventsFiles)) {
+#   
+#   tsv <- read.table(paste(bidsRoot, taskEventsFiles[i], sep = '/'), header = TRUE)
+#   
+#   # control point = subjectCue already in the column, then do not do it again
+#   if (length(grep('block_forehead2', tsv$trial_type)) !=0){
+#     print('...in progress')
+#     
+# 
+#     # subset the data & remove "2" from forehead condition
+#     tsv$trial_type <- ifelse(tsv$trial_type=='block_forehead2', 'block_forehead',tsv$trial_type)
+#    
+#     # save into .tsv file
+#     write.table(tsv,
+#                 paste(bidsRoot, taskEventsFiles[i], sep = '/'),
+#                 row.names = FALSE,
+#                 sep = '\t',
+#                 quote = FALSE)
+#   } else {
+#     print('nothing to do here')
+#   }
+#   
+# }
