c483418e99fd2d3d560ce9a9b3ba21501b80ac86
Ceren <battal.ceren@gmail.com>
2024-07-09 14:30:41 +0200

    formatting

--- src/copySourceToRawAndOmitSuffix.m
+++ src/copySourceToRawAndOmitSuffix.m
@@ -1,17 +1,28 @@
-function copySourceToRawAndOmitSuffix(subject)
+function copySourceToRawAndOmitSuffix(opt)
 
 % remove date suffix + copy source to raw 
 % add bids repo
 run ../lib/CPP_BIDS/checkCppBidsDependencies.m;
 
-% remove data suffix
-filter = struct('sub', subject);
+% % % NOTE : this way does not copy/paste the anatomy scan
 
-% copy source to raw + remove date suffix in raw folder
-cfg.dir.output = '/Users/battal/Cerens_files/fMRI/Processed/MoebiusProject/';
-convertSourceToRaw(cfg, 'filter', filter)
 
+taskName = opt.taskName; 
+
+for iSub = 1:length(opt.subjects)
+    
+    subject = opt.subjects{iSub};
+    
+    % remove data suffix
+    filter = struct('sub', subject, ...
+                'task', taskName);
+
+    % copy source to raw + remove date suffix in raw folder
+    cfg.dir.output = '/Users/battal/Cerens_files/fMRI/Processed/MoebiusProject/';
+    convertSourceToRaw(cfg, 'filter', filter)
+
+end
 % remove cpp-bids repo now to prevent repos compatibility issues
-rmpath(genpath('../lib/CPP_BIDS/'));
+% rmpath(genpath('../lib/CPP_BIDS/'));
 
 end
